20b7453611be76aa46f88d1d4fb23b6356f0b3be
bressack <tharchen@student.42.fr>
2021-06-16 02:54:44 +0200

    auto commit from alias

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tharchen <tharchen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/22 12:15:04 by tharchen          #+#    #+#             */
-/*   Updated: 2021/06/16 02:36:19 by tharchen         ###   ########.fr       */
+/*   Updated: 2021/06/16 02:54:39 by tharchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	main(int ac, char **av)
 	if (ac < 5 || ac > 6)
 		printf("usage: %s %s%s\n", av[0], USAGE, USAGE2);
 	if (init(&info, ac, av) == FAILURE)
-		return (FAILURE);
+		exit(FAILURE);
 	if (simulation(&info) == FAILURE)
-		return (FAILURE);
+		exit(FAILURE);
 	return (0);
 }
